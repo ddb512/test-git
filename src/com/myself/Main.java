6598c8b26f99330a7ecf1b9c805990378bcdd6f0
2959420527@qq.com <2959420527@qq.com>
2021-08-22 16:27:37 +0800

    分支8-22提交idea中的修改

--- src/com/myself/Main.java
+++ src/com/myself/Main.java
@@ -5,6 +5,8 @@ public class Main {
     //我是本地git管理
     //本地git第二次提交
     public static void main(String[] args) {
-	    System.out.println("Hello Git");
+
+        System.out.println("Hello Git");
+        System.out.println("在idea中修改");
     }
 }
